4543fbd2a45b51cff8e84640c4f3fc137a861c3f
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-06-21 17:02:07 +0200

    category filter & search

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,40 @@
 import './Categories.css'
+import { Link } from 'react-router-dom';
+import { useState, useContext, useEffect } from 'react';
+import { CategoryFilterContext } from '../context/Context'
 
 const Categories = () => {
+    const [categories, setCategories] = useState([])
+    const {categoryFilter, setCategoryFilter} = useContext(CategoryFilterContext)
+
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then((res) => res.json())
+        .then((data) => setCategories(data.categories))
+        .catch((err) => console.log(`Fehler: ${err}`))
+    },[])
+
     return ( 
-        <>
-        <h1>Categories</h1>
-        </>
+        <section className='categories'>
+            <div>
+                <h4>Categories</h4>
+                <Link to='/search/category'>See All</Link>
+            </div>
+            <figure className='categories-wrapper'>
+            {categories ? (
+                categories.map((category, index) => {return (
+                <Link key={index} to='/search/category' >
+                    <article onClick={() => setCategoryFilter(category.strCategory)} key={index}>
+                    <img src={category.strCategoryThumb} alt={category.strCategory} />
+                    <h5>{category.strCategory}</h5>
+                    </article>
+                </Link>
+            )})
+            ) : (
+                <p>loeading data...</p>
+            )}
+            </figure>
+        </section>
      );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ import './Home.css'
 const Home = () => {
     return ( 
         <>
-        <h1>Home </h1>
         <Search/>
         <RandomMeal/>
         <AreasBtnSection/>
