2f59df6641b211a7f1c9fcbac4b898314f9cdd2c
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-06-22 11:06:10 +0200

    media queries random meal

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,9 +1,11 @@
+import RandomMeal from '../components/RandomMeal';
 import './Onboarding.css'
 
 const Onboarding = () => {
     return ( 
         <>
         <h1>Onboarding</h1>
+        <RandomMeal/>
         </>
      );
 }
