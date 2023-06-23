21775632ef026a3071ef804b64d920ef57ba5f11
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-06-23 13:54:13 +0200

    fixed Darkmode, added second Context for Darkmode on Detailspage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './pages/Home'
-import Onboarding from './pages/Onboarding'
-import SearchInput from './pages/SearchInput'
-import SearchAreas from './pages/SearchAreas'
-import SearchCategory from './pages/SearchCategory'
-import Details from './pages/Details'
-import LoadingSection from './components/LoadingSection'
-import { useEffect, useState } from 'react'
-import { CategoryFilterContext, SearchbarCategoryContext, FilteredAreaContext, SearchTermAreaContext, SearchTermAllProductsContext, ThemeContext } from './context/Context'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import SearchInput from "./pages/SearchInput";
+import SearchAreas from "./pages/SearchAreas";
+import SearchCategory from "./pages/SearchCategory";
+import Details from "./pages/Details";
+import LoadingSection from "./components/LoadingSection";
+import { useEffect, useState } from "react";
+import {
+  CategoryFilterContext,
+  SearchbarCategoryContext,
+  FilteredAreaContext,
+  SearchTermAreaContext,
+  SearchTermAllProductsContext,
+  ThemeContext,
+  ThemeDetailContext,
+} from "./context/Context";
 
 function App() {
   const [theme, setTheme] = useState(false);
-  const [loading, setLoading] = useState()
-  const [categoryFilter, setCategoryFilter] = useState("Beef")
-  const [searchInputCategory, setSearchInputCategory] = useState("")
+  const [themeDetailPage, setThemeDetailPage] = useState(false);
+  const [loading, setLoading] = useState();
+  const [categoryFilter, setCategoryFilter] = useState("Beef");
+  const [searchInputCategory, setSearchInputCategory] = useState("");
   const [filteredArea, setFilteredArea] = useState([]);
   const [searchInputArea, setSearchInputArea] = useState("");
   const [searchInputAllProducts, setsearchInputAllProducts] = useState("");
@@ -22,34 +31,57 @@ function App() {
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 4000)
-  }, [])
+      setLoading(false);
+    }, 4000);
+  }, []);
 
   return (
-    <section className={theme ? "dark" : ""}>
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-    <FilteredAreaContext.Provider value={{ filteredArea, setFilteredArea }}>
-    <SearchTermAreaContext.Provider value={{ searchInputArea, setSearchInputArea }}>
-    <SearchTermAllProductsContext.Provider value={{ searchInputAllProducts, setsearchInputAllProducts }}>
-    <SearchbarCategoryContext.Provider value={{ searchInputCategory, setSearchInputCategory }}>
-    <CategoryFilterContext.Provider value={{ categoryFilter, setCategoryFilter }}>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={loading ? <LoadingSection /> : <Onboarding/>}/>
-      <Route path='/home' element={<Home/>}/>
-      <Route path='/search/input' element={<SearchInput/>}/>
-      <Route path='/search/areas' element={<SearchAreas/>}/>
-      <Route path='/search/category' element={<SearchCategory/>}/>
-      <Route path='/detail/:id' element={<Details/>}/>
-    </Routes>
-    </BrowserRouter>
-    </CategoryFilterContext.Provider>
-    </SearchbarCategoryContext.Provider>
-    </SearchTermAllProductsContext.Provider>
-    </SearchTermAreaContext.Provider>
-    </FilteredAreaContext.Provider>
-    </ThemeContext.Provider>
+    <section className={theme ? "dark" : "light"}>
+      <ThemeDetailContext.Provider
+        value={{ themeDetailPage, setThemeDetailPage }}
+      >
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+          <FilteredAreaContext.Provider
+            value={{ filteredArea, setFilteredArea }}
+          >
+            <SearchTermAreaContext.Provider
+              value={{ searchInputArea, setSearchInputArea }}
+            >
+              <SearchTermAllProductsContext.Provider
+                value={{ searchInputAllProducts, setsearchInputAllProducts }}
+              >
+                <SearchbarCategoryContext.Provider
+                  value={{ searchInputCategory, setSearchInputCategory }}
+                >
+                  <CategoryFilterContext.Provider
+                    value={{ categoryFilter, setCategoryFilter }}
+                  >
+                    <BrowserRouter>
+                      <Routes>
+                        <Route
+                          path="/"
+                          element={
+                            loading ? <LoadingSection /> : <Onboarding />
+                          }
+                        />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/search/input" element={<SearchInput />} />
+                        <Route path="/search/areas" element={<SearchAreas />} />
+                        <Route
+                          path="/search/category"
+                          element={<SearchCategory />}
+                        />
+
+                        <Route path="/detail/:id" element={<Details />} />
+                      </Routes>
+                    </BrowserRouter>
+                  </CategoryFilterContext.Provider>
+                </SearchbarCategoryContext.Provider>
+              </SearchTermAllProductsContext.Provider>
+            </SearchTermAreaContext.Provider>
+          </FilteredAreaContext.Provider>
+        </ThemeContext.Provider>
+      </ThemeDetailContext.Provider>
     </section>
   );
 }
--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,42 +1,41 @@
-import './Instructions.css'
+import "./Instructions.css";
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
 const Instructions = () => {
-    
-    const [instructionData, setInstructionData] = useState();
-    const params = useParams();
-    const idDish = params.id;
+  const [instructionData, setInstructionData] = useState();
+  const params = useParams();
+  const idDish = params.id;
 
-    useEffect(() => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idDish}`)
-            .then(res => res.json())
-            .then(instructionData => {
-                setInstructionData(instructionData.meals[0])
-            })
-            .catch(error => {
-                console.error("Fehler beim Fetch", error);
-            });
-    }, []);  
-    
-    const formatText = instructionData?.strInstructions.split(`\r\n`);
-    const absatz = formatText?.map((item, index) => (
-        <div key={index} className='InstructionsAbsatz'>
-            <p >{item}</p> <br/>
-        </div>
-    )) 
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idDish}`)
+      .then((res) => res.json())
+      .then((instructionData) => {
+        setInstructionData(instructionData.meals[0]);
+      })
+      .catch((error) => {
+        console.error("Fehler beim Fetch", error);
+      });
+  }, []);
 
-    const youTubeURL = instructionData?.strYoutube;
-   
-    return ( 
-        <>
-        <h1>Instructions</h1>
-        <section className='InstructionsContainer'>
-            {absatz} 
-        </section>
-        <Link to={youTubeURL}><button>Video</button></Link> 
-        </>
-    );
-}
- 
+  const formatText = instructionData?.strInstructions.split(`\r\n`);
+  const absatz = formatText?.map((item, index) => (
+    <div key={index} className="InstructionsAbsatz">
+      <p>{item}</p> <br />
+    </div>
+  ));
+
+  const youTubeURL = instructionData?.strYoutube;
+
+  return (
+    <>
+      <h1>Instructions</h1>
+      <section className="InstructionsContainer">{absatz}</section>
+      <Link to={youTubeURL}>
+        <button>Video</button>
+      </Link>
+    </>
+  );
+};
+
 export default Instructions;
