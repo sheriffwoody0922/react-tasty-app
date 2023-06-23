7f1de2c5f2eb6662274e2d7cb6d84b5528c625ed
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-06-23 09:26:56 +0200

    nav korrektur

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import './Nav.css'
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Home from '../images/nav-icon/Home.svg';
 import Search from '../images/nav-icon/Search.svg';
@@ -8,26 +8,57 @@ import Profile from '../images/nav-icon/Profile.svg';
 import Darkmode from '../images/nav-icon/Darkmode';
 import { useContext } from "react";
 import { ThemeContext  } from '../context/Context';
+import HomeActive from '../images/nav-icon/HomeActive.svg'
+import SearchActive from '../images/nav-icon/SearchActive.svg'
+import HeartActive from '../images/nav-icon/HeartActive.svg'
+import ProfileActive from '../images/nav-icon/ProfileActive.svg'
 
 const Nav = () => {
    const { theme, setTheme } = useContext(ThemeContext);
+   const [btnClicked, setBtnClicked] = useState("home")
+
    const toggleTheme = () => {
       setTheme((darkMode) => !darkMode);
     };
+
     return (
        <section className='wrapper-nav'>
         <nav className='navBar'>
-           <NavLink to="/home"><img src={Home} alt="Home-Button"/></NavLink>
-           <NavLink to="/search/input"><img src={Search} alt="Search-Button" /></NavLink>
-           <NavLink><img src={Heart} alt="like-Button" /></NavLink>
-           <NavLink><img src={Profile} alt="Profile-Button" /></NavLink>
+
+           <NavLink onClick={() => setBtnClicked("home")} to="/home">
+            {btnClicked == "home" ? (
+               <img src={HomeActive} alt="Home-Button" />
+            ) : (
+               <img src={Home} alt="Home-Button" />
+            )}
+            </NavLink>
+           <NavLink onClick={() => setBtnClicked("search")} to="/search/input">
+            {btnClicked == "search" ? (
+               <img src={SearchActive} alt="Search-Button" />
+            ) : (
+               <img src={Search} alt="Search-Button" />
+            )}
+            </NavLink>
+           <NavLink onClick={() => setBtnClicked("like")}>
+           {btnClicked == "like" ? (
+            <img src={HeartActive} alt="like-Button" />
+           ) : (
+            <img src={Heart} alt="like-Button" />
+           )}
+            </NavLink>
+           <NavLink onClick={() => setBtnClicked("profile")}>
+           {btnClicked == "profile" ? (
+            <img src={ProfileActive} alt="Profile-Button" />
+           ) : (
+            <img src={Profile} alt="Profile-Button" />
+           )}
+            </NavLink>
+
            <div onClick={toggleTheme}>
             <Darkmode />
-          </div>
+          </div>        
         </nav>
-        <div className='wrapper-block'>
-        <div className='block'></div>
-        </div>
+         <div className='block'></div>
         </section>
      );
 }
