1b839cd167a597e27c470cd6fb5ad86343ea6057
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-06-23 13:38:57 +0200

    longmeal test

--- src/components/SearchCategoryItem.jsx
+++ src/components/SearchCategoryItem.jsx
@@ -3,45 +3,46 @@ import './SearchCategoryItem.css'
 
 const SearchCategoryItem = (props) => {
     // const [mealName, setMealName] = useState()
-    const [screenWidth, setScreenWidth] = useState()
-    const [mobile, setMobile] = useState(true)
-    const [desktop, setDesktop] = useState(false)
+    // const [screenWidth, setScreenWidth] = useState()
+    // const [mobile, setMobile] = useState(true)
+    // const [desktop, setDesktop] = useState(false)
 
 let shortMeal = props.meal.strMeal
 shortMeal = shortMeal.slice(0,9)
 shortMeal = `${shortMeal}...`
 
-let longMeal = props.meal.strMeal
+// let longMeal = props.meal.strMeal
 
-window.onresize = function () {
-    if(window.screen.width < 567){
-        // setMealName(shortMeal)
-        // setScreenWidth("mobile")
-        setMobile(true)
-        setDesktop(false)
-        // console.log({screenWidth});
-        console.log({mobile});
-    }
-    if(window.screen.width > 567){
-        // setMealName(longMeal)
-        // setScreenWidth("desktop")
-        setDesktop(true)
-        setMobile(false)
-        // console.log({screenWidth});
-        console.log({desktop});
-    }
-    console.log("resized");
-}
+// window.onresize = function () {
+//     if(window.screen.width < 567){
+//         // setMealName(shortMeal)
+//         // setScreenWidth("mobile")
+//         setMobile(true)
+//         setDesktop(false)
+//         // console.log({screenWidth});
+//         console.log({mobile});
+//     }
+//     if(window.screen.width > 567){
+//         // setMealName(longMeal)
+//         // setScreenWidth("desktop")
+//         setDesktop(true)
+//         setMobile(false)
+//         // console.log({screenWidth});
+//         console.log({desktop});
+//     }
+//     console.log("resized");
+// }
 
 // console.log(window.screen.width);
 
     return ( 
         <article className='search-category-item'>
             <img src={props.meal.strMealThumb} alt={props.meal.strMeal} />
+            <p>{shortMeal}</p>
 
-            <p style={mobile ? {display: "block"} : {display: "none"}}>{shortMeal}</p>
+            {/* <p style={mobile ? {display: "block"} : {display: "none"}}>{shortMeal}</p>
          
-            <p style={desktop ? {display: "block"} : {display: "none"}}>{longMeal}</p>
+            <p style={desktop ? {display: "block"} : {display: "none"}}>{longMeal}</p> */}
             
         </article>
      );
