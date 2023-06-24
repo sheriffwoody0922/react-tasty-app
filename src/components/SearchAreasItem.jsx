28b3f115a8bcdc0cc4e6e027cfd9477e8938d2b8
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-06-24 22:08:21 +0200

    added long texts and css

--- src/components/SearchAreasItem.jsx
+++ src/components/SearchAreasItem.jsx
@@ -1,17 +1,31 @@
+import { useEffect, useState } from 'react'
 import "./SearchAreasItem.css";
 
 const SearchAreasItem = (props) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 567)
 
-  let shortMeal = props.meal.strMeal
-  shortMeal = shortMeal.slice(0,9)
-  shortMeal = `${shortMeal}...`
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 567)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  const shortMeal = props.meal.strMeal.slice(0, 9) + '...'
+  const longMeal = props.meal.strMeal
 
   return (
-      <article className="area-meal-item" value={props.meal.idMeal}>
+      <article className="area-meal-item" >
           <div className="areaImageBox">
             <img src={props.meal.strMealThumb} alt={props.meal.strMeal} />
           </div>
-          <p className="meal-name-area">{shortMeal}</p>
+          <div className='meal-name-wrapper'>
+        <p className='meal-name-area' style={isMobile ? { display: 'flex' } : { display: 'none' }}>{shortMeal}</p>
+        <p className='meal-name-area' style={!isMobile ? { display: 'flex' } : { display: 'none' }}>{longMeal}</p>
+    </div>
       </article>
   );
 };
